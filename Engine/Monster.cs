fe6ead736af4c1821527777fbb550c47bab85df0
ManeDM <mane657@hotmail.com>
2023-07-12 15:18:40 -0500

    Remaining Classes

---
+++ Engine/Monster.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    internal class Monster
+    {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public int MaximunHitPoints { get; set; }
+        public int CurrentHitPoints { get; set; }
+        public int MaximunDamage {get; set; }
+        public int RewardEXperiencePoints { get; set; }
+        public int RewardGold { get; set; }
+    }
+}
