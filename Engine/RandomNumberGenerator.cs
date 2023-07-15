0260fd0e51469e490e53f759c0aadc2b39fd4b08
ManeDM <mane657@hotmail.com>
2023-07-15 13:21:45 -0500

    Random Numbers Generator

---
+++ Engine/RandomNumberGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public static class RandomNumberGenerator
+    {
+        private static Random _generator = new Random();
+        public static int NumberBetween(int minimunValue, int maximunValue)
+        {
+            return _generator.Next(minimunValue, maximunValue + 1);
+        }
+    }
+}
